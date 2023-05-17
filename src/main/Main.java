507fc156081b5ed3bbcdcabcf4287c70169f44e0
JDuffy135 <122391255+JDuffy135@users.noreply.github.com>
2023-05-17 15:17:56 -0400

    added JPanel

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.JFrame;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("PACMAN REMAKE");
+
+        gamePanel panel = new gamePanel();
+        window.add(panel);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
